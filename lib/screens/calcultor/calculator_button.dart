6bfa8446045121f0b2f5aca2d5f9acbde8fbb3be
Your Name <you@example.com>
2025-02-09 19:45:29 +0200

    Changed font size

--- lib/screens/calcultor/calculator_button.dart
+++ lib/screens/calcultor/calculator_button.dart
@@ -25,7 +25,7 @@ class CalculatorButton extends StatelessWidget {
           child: Text(
             digit,
             style: TextStyle(
-              fontSize: 30,
+              fontSize: 32,
               color: Colors.red,
             ),
           ),
